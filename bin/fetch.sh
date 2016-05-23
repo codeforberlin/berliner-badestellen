3ea0449706306e93a67bd2ef289f3e31a35b45de
Magdalena Noffke <magdalena.noffke@liqd.de>
2016-05-23 20:59:18 +0200

    fix fetch.sh

--- bin/fetch.sh
+++ bin/fetch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd public/data
 mv all.json all.json-
-wget http://www.berlin.de/lageso/gesundheit/gesundheitsschutz/badegewaesser/liste-der-badestellen/index.php/index.json?q=
+wget http://www.berlin.de/lageso/gesundheit/gesundheitsschutz/badegewaesser/liste-der-badestellen/index.php/index.json?q= -O all.json
